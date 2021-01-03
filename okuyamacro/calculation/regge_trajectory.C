9139c06fef762acf3c688e2deb19f432067cc11f
okuyama <kazuki@lambda.phys.tohoku.ac.jp>
2021-01-03 21:05:26 +0900

    regge trajectory

---
+++ okuyamacro/calculation/regge_trajectory.C
@@ -0,0 +1,113 @@
+void regge_trajectory(){
+gROOT->Reset();
+gROOT->SetStyle("Plain");
+
+TCanvas* c1 = new TCanvas("c1", "c1", 800, 600);
+//c1->SetLeftMargin(0.15);
+//c1->SetBottomMargin(0.15);
+//c1->SetTopMargin(0.15);
+//c1->SetRightMargin(0.15);
+//c1->Draw();
+
+
+//TH2F* h1 = c1 -> DrawFrame(500.,1000.,1000.,2000.);
+//h1->GetXaxis()->SetTitleFont(12);
+//h1->GetXaxis()->SetTitleSize(0.05);
+//h1->GetXaxis()->SetTitleOffset(1.05);
+//h1->GetXaxis()->SetTitle("p [MeV]");
+//h1->GetXaxis()->SetLabelOffset(0.02);
+//h1->GetYaxis()->SetTitleFont(22);
+//h1->GetYaxis()->SetTitleOffset(1.1);
+//h1->GetYaxis()->SetTitleSize(0.05);
+//h1->GetYaxis()->SetTitle("1/#beta");
+
+double x,y;
+double M = 0.9382720;
+double k_family_x[5];
+double k_family_y[5];
+double k_family_xe[5];
+double k_family_ye[5];
+double ks_family_x[5];
+double ks_family_y[5];
+double ks_family_xe[5];
+double ks_family_ye[5];
+k_family_x[0]=0.494*0.494;//K^+(494)
+k_family_x[1]=1.270*1.270;//K_1(1270)
+k_family_x[2]=1.770*1.770;//K_2(1770)
+k_family_x[3]=2.320*2.320;//K_4(2320)
+k_family_x[4]=2.500*2.500;//K_5(2500)
+k_family_y[0]=0.;
+k_family_y[1]=1.;
+k_family_y[2]=2.;
+k_family_y[3]=3.;
+k_family_y[4]=4.;
+k_family_xe[0]=0.;
+k_family_xe[1]=0.;
+k_family_xe[2]=0.;
+k_family_xe[3]=0.;
+k_family_xe[4]=0.;
+k_family_ye[0]=0.;
+k_family_ye[1]=0.;
+k_family_ye[2]=0.;
+k_family_ye[3]=0.;
+k_family_ye[4]=0.;
+//%%%%%%%%%%%%%%%%%%%%%%%%//
+ks_family_x[0]=0.892*0.892;//K^*(892)
+ks_family_x[1]=1.430*1.430;//K_2^*(1430)
+ks_family_x[2]=1.780*1.780;//K_3^*(1780)
+ks_family_x[3]=2.045*2.045;//K_4^*(2045)
+ks_family_x[4]=2.380*2.380;//K_5^*(2380)
+ks_family_y[0]=1.;
+ks_family_y[1]=2.;
+ks_family_y[2]=3.;
+ks_family_y[3]=4.;
+ks_family_y[4]=5.;
+ks_family_xe[0]=0.;
+ks_family_xe[1]=0.;
+ks_family_xe[2]=0.;
+ks_family_xe[3]=0.;
+ks_family_xe[4]=0.;
+ks_family_ye[0]=0.;
+ks_family_ye[1]=0.;
+ks_family_ye[2]=0.;
+ks_family_ye[3]=0.;
+ks_family_ye[4]=0.;
+
+  TGraphErrors *gr_kfamily = new TGraphErrors(5, k_family_x, k_family_y, k_family_xe, k_family_ye);
+  gr_kfamily->SetMarkerSize(1.5);
+  gr_kfamily->SetMarkerStyle(21);
+  gr_kfamily->SetFillStyle(3004);
+  //gr_kfamily->SetLineColor(2);
+  gr_kfamily->SetFillColor(kRed);
+  gr_kfamily->SetMarkerColor(2);
+  TGraphErrors *gr_ksfamily = new TGraphErrors(5, ks_family_x, ks_family_y, ks_family_xe, ks_family_ye);
+  gr_ksfamily->SetMarkerSize(1.5);
+  gr_ksfamily->SetMarkerStyle(21);
+  gr_ksfamily->SetFillStyle(3004);
+  //gr_ksfamily->SetLineColor(2);
+  gr_ksfamily->SetFillColor(kAzure);
+  gr_ksfamily->SetMarkerColor(kAzure);
+
+
+
+TH1F* h1 = c1 -> DrawFrame(0.,0.,7.,7.);
+//c1->SetGrid(0);
+//h1->GetXaxis()->SetTitleFont(12);
+h1->GetXaxis()->SetTitleSize(0.05);
+h1->GetXaxis()->SetTitleOffset(1.05);
+h1->GetXaxis()->SetTitle("t [GeV^{2}]");
+h1->GetXaxis()->SetLabelOffset(0.02);
+//h1->GetYaxis()->SetTitleFont(22);
+h1->GetYaxis()->SetTitleOffset(0.9);
+h1->GetYaxis()->SetTitleSize(0.05);
+h1->GetYaxis()->SetTitle("#alpha(t)");
+	gPad->SetLeftMargin(0.15);
+	gPad->SetRightMargin(0.15);
+	gPad->SetTopMargin(0.15);
+	gPad->SetBottomMargin(0.15);
+c1->Update();
+
+gr_kfamily->Draw("Psame");
+gr_ksfamily->Draw("Psame");
+
+}
--- root_Seminar/mthesis_uniform.cc
+++ root_Seminar/mthesis_uniform.cc
@@ -1,16 +1,28 @@
 #include <TMath.h>
-void test_uniform(){
+void mthesis_uniform(){
+  double PI = 4.*atan(1.);
+
   TH3D *h3_test1 = new TH3D("h3_test1","h3_test1",100,-1.,1.,100.,-1.,1.,100.,-1.,1.);
   //TH3D *h3_test2 = new TH3D("h3_test2","h3_test2",100,-1.,1.,100.,-1.,1.,100.,-1.,1.);
   TH3D *h3_test2 = new TH3D("h3_test2","",100,-1.,1.,100.,-1.,1.,100.,-1.,1.);
+  h3_test2->SetNdivisions(505,"XYZ");
+  h3_test2->GetXaxis()->SetTitle("x");
+  h3_test2->GetXaxis()->SetTitleSize(0.06);
+  h3_test2->GetXaxis()->CenterTitle();
+  h3_test2->GetYaxis()->SetTitle("y");
+  h3_test2->GetYaxis()->SetTitleSize(0.06);
+  h3_test2->GetYaxis()->CenterTitle();
+  h3_test2->GetZaxis()->SetTitle("z");
+  h3_test2->GetZaxis()->SetTitleSize(0.06);
+  h3_test2->GetZaxis()->SetTitleOffset(0.8);
+  h3_test2->GetZaxis()->CenterTitle();
   TH3D *h3_test3 = new TH3D("h3_test3","h3_test3",100,-1.,1.,100.,-1.,1.,100.,-1.,1.);
-  TH1D *h_test1  = new TH1D("h_test1","h_test1",1000,0.,4.);
-  TH1D *h_test2  = new TH1D("h_test2","h_test2",1000,0.,4.);
-  TH1D *h_test3  = new TH1D("h_test3","h_test3",1000,0.,4.);
+  TH1D *h_test1  = new TH1D("h_test1","h_test1",1000,0.,PI);
+  TH1D *h_test2  = new TH1D("h_test2","h_test2",1000,0.,PI);
+  TH1D *h_test3  = new TH1D("h_test3","h_test3",1000,0.,PI);
 //  SetTH1(h,"homework3","x","y");
   gStyle->SetOptStat(0);
 
-  double PI = 4.*atan(1.);
 cout<<"PI="<<PI<<endl;
   gRandom->SetSeed(0);
   int N1 = 0;
@@ -20,7 +32,7 @@ cout<<"PI="<<PI<<endl;
 
 	double theta_gen1 = acos( -(2.0*gRandom->Uniform()*-1.0)*gRandom->Uniform());//SIMC
 	double theta_gen2 = acos(gRandom->Uniform(-1.,1.));//UNIFORM
-	double theta_gen3 = acos(1.-gRandom->Uniform()*(1.-cos(PI/2.)));//uniform in 0--pi/2 rad
+	double theta_gen3 = acos(1.-gRandom->Uniform()*(1.-cos(0.1)));//uniform in 0--pi/2 rad
 	double phi_gen = 2.*PI*gRandom->Uniform(0.,1.);
 	double x1 = sin(theta_gen1)*cos(phi_gen);
 	double y1 = sin(theta_gen1)*sin(phi_gen);
@@ -51,16 +63,14 @@ cout<<"N3="<<N3<<endl;
   c1->cd();
   h_test1->Scale((double)N2/(double)N1);
   h_test3->Scale((double)N2/(double)N3);
-  h_test1->Draw();
+  //h_test1->Draw();
   h_test2->SetLineColor(kRed);
   h_test3->SetLineColor(kGreen);
   h_test2->Draw("same");
   h_test3->Draw("same");
-//  TCanvas *c2 = new TCanvas("c2","c2",600,600);
-//  h3_test1->Draw("");
+
   TCanvas *c3 = new TCanvas("c3","c3",600,600);
   h3_test2->Draw("");
-//  TCanvas *c4 = new TCanvas("c4","c4",600,600);
-//  h3_test3->Draw("");
+//  c3->Print("uniform_3d.pdf");
 
 }
